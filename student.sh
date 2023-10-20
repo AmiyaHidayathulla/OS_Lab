1f800e02f64135da114d767d25476a6df28242d1
Amiya Hidayathulla <95171763+AmiyaHidayathulla@users.noreply.github.com>
2023-10-21 00:03:15 +0530

    Create student.sh

---
+++ student.sh
@@ -0,0 +1,11 @@
+echo Enter your name:
+read name
+echo Enter your Semester:
+read sem
+echo Enter your batch:
+read batch
+echo Enter your roll no::
+read no
+echo Enter your Register number:
+read regno
+echo -e "Name:$name\nSemester:$sem\nBatch:$batch\nRoll No.:$no\nRegister No.:$regno"
